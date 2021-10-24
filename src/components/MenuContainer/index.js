4dbe10f2a75efb90b59b5c6b8d8c8276e45c0d42
Juju <juju@ribon.io>
2021-10-24 07:46:30 +0100

    Fix menu switch

--- src/components/MenuContainer/index.js
+++ src/components/MenuContainer/index.js
@@ -1,17 +1,31 @@
-import { useBlockchain } from "../../contexts/blockchain";
+import { useLocation } from "react-router-dom";
 import useNavigation from "../../hooks/useNavigation";
 import "./index.css"
 
 export default function HeaderContainer() {
   const { navigateTo } = useNavigation();
+  const location = useLocation();
 
   const navigateDeposit = () => navigateTo({pathname: "/"});
   const navigateWithdraw = () => navigateTo({pathname: "/withdraw"});
 
+  const isInDepositPath = ["/", "/deposit"].includes(
+    location.pathname,
+  );
+  const isInWithdrawPath = ["/withdraw"].includes(
+    location.pathname,
+  );
+
+  const depositActiveItem = <div className="MenuItem -active">Deposit</div>
+  const depositItem = <div className="MenuItem"  onClick={() => navigateDeposit()}>Deposit</div>
+
+  const withdrawActiveItem = <div className="MenuItem -active">Withdraw</div>
+  const withdrawItem = <div className="MenuItem" onClick={() => navigateWithdraw()}>Withdraw</div>
+  
   return (
     <div className="MenuContainer">
-      <div className="MenuItem" onClick={() => navigateDeposit()}>Deposit</div>
-      <div className="MenuItem" onClick={() => navigateWithdraw()}>Withdraw</div>
+      {isInDepositPath ? depositActiveItem : depositItem}
+      {isInWithdrawPath ? withdrawActiveItem : withdrawItem}
     </div>
   )
 }
